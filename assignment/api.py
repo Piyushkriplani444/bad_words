423230b44b79ef4ee3fec0900cccd6714d89edee
Piyush Kriplani <piyushkriplani444@gmail.com>
2022-04-21 15:10:55 +0530

    added flask api to run check bad words

---
+++ assignment/api.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+from check_bad_word import check_bad_word
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def home():
+	if(request.method == 'GET'):
+		data = "Bad Word Api is Running Correctly"
+		return jsonify({'data':data})
+
+
+@app.route('/bad_words/',methods=['POST'])
+def badwords():
+	if(request.method == 'POST'):
+		request_data = request.get_json()
+		text = request_data['text']
+		result, bad_bool = check_bad_word(text, './data/badwords.txt')
+		bad_word_response = {
+		'text': result,
+		'bad': bad_bool
+		}
+
+		return jsonify(bad_word_response)
+
+
+if __name__ == "__main__":
+	app.run(debug = True)
